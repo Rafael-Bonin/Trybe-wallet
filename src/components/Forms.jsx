3488af82474246cea8eaf2e1c905a5e0454ccb1f
Rafael <rafalyf11@gmail.com>
2022-02-10 16:37:01 -0300

    tentando corrigir

---
+++ src/components/Forms.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Forms extends Component {
+  render() {
+    return (
+      <form>
+        <input type="number" data-testid="value-input" />
+      </form>
+    )
+  }
+}
